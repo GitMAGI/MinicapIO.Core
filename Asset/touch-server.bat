2bb148077fc0eee0ae3cca24970cd9cd913fffb3
Hikari Genzo <leonxp@libero.it>
2019-08-15 16:54:34 +0200

    Touch Preliminare

--- Asset/touch-server.bat
+++ Asset/touch-server.bat
@@ -1,14 +1,28 @@
 @echo off
 
 ::Samsung S5
-SET device = 07ea9707 
+::SET device = 07ea9707 
 
- ::OnePlus3T
-::SET device = 743580a0
+::OnePlus3T
+SET device = 743580a0
 
 ::C:\android-sdk\tools\android-ndk-r14b\
 ::C:\android-sdk\platform-tools\adb.exe -s "%device%"
 
 ::To Restart The Device
 ::C:\android-sdk\platform-tools\adb.exe shell am broadcast -a android.intent.action.BOOT_COMPLETED
+
+C:\android-sdk\platform-tools\adb.exe -s "%device%" forward tcp:9889 tcp:9889
+
+C:\android-sdk\platform-tools\adb.exe -s "%device%" push touch/libs/armeabi-v7a/touch /data/local/tmp/
+
+C:\android-sdk\platform-tools\adb.exe -s "%device%" shell chmod 777 /data/local/tmp/touch
+
+C:\android-sdk\platform-tools\adb.exe -s "%device%" shell /data/local/tmp/touch
+
+::To Log
+::C:\android-sdk\platform-tools\adb.exe -s "%device%"  logcat | find "touch"
+
+C:\android-sdk\platform-tools\adb.exe -s "%device%" forward --remove tcp:9889
+
 
