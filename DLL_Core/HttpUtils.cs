e57c7c07cadf97c50aa149b81c821b9f7ee1388c
Hikari Genzo <leonxp@libero.it>
2019-08-21 15:15:29 +0200

    Metodo Call on Socket è performante!

--- DLL_Core/HttpUtils.cs
+++ DLL_Core/HttpUtils.cs
@@ -41,49 +41,6 @@ namespace DLL_Core
             return httpResponse;
         }
 
-        public static int JHttpPost(Socket socket, string path, string payload, HttpStatusCode success = HttpStatusCode.OK)
-        {
-            string response = JHttpRequest(socket, "POST", path, payload);
-            HttpStatusCode code = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), response.Split(' ')[1]);
-            if (code == success)
-                return 0;
-            else
-                return -1;
-        }
-
-        private static string JHttpRequest(Socket socket, string method, string path, string payload)
-        {
-            IPEndPoint RemoteIpEndPoint = socket.RemoteEndPoint as IPEndPoint;
-            string RemoteIP = RemoteIpEndPoint.Address.ToString();
-            int RemotePort = RemoteIpEndPoint.Port;
-
-            List<string> headers = new List<string>()
-            {
-                string.Format("{0} {1} HTTP/1.1", method, path),
-                string.Format("{0}: {1}:{2}", "Host", RemoteIP, RemotePort),
-                string.Format("{0}: {1}", "Content-Type", "application/json"),
-                string.Format("{0}: {1}", "cache-control", "no-cache"),
-                string.Format("{0}: {1}", "Content-Length", Encoding.UTF8.GetByteCount(payload))
-            };
-            string request = string.Join("\r\n", headers) + "\r\n" + "\r\n" + payload;
-            byte[] dataToSend = Encoding.UTF8.GetBytes(request);
-
-            int bytesSent = socket.Send(dataToSend, dataToSend.Length, SocketFlags.None);
-
-            string httpResponse = "";
-            
-            int bytesReceived = 0;
-            byte[] dataReceived = new byte[10000];            
-            do
-            {
-                bytesReceived = socket.Receive(dataReceived, SocketFlags.None);
-                httpResponse = httpResponse + Encoding.ASCII.GetString(dataReceived, 0, bytesReceived);
-            }
-            while (bytesReceived > 0);
-            
-            return httpResponse;
-        }
-
         public static int JHttpPost(string remoteIP, int remotePort, string path, string payload, HttpStatusCode success = HttpStatusCode.OK)
         {
             string response = JHttpRequest(remoteIP, remotePort, "POST", path, payload);
@@ -145,6 +102,5 @@ namespace DLL_Core
 
             return httpResponse;
         }
-
     }
 }
